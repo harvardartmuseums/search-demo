ef7dd3fbb16de1c8e91f9fc43c98e3d7b605206c
Ian Callahan <callahan@hey.com>
2021-06-23 14:01:54 -0400

    Update assets

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative flex w-full h-full bg-white p-52">
+    <div class="relative flex w-full h-full bg-white p-2 md:p-52 justify-center">
         <collections-search context="search" :url="null" placeholder="Enter search terms" :filter_group_id="null"/>
     </div>
 </template>
